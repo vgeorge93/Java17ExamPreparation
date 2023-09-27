d480d6f18078d8117cf8e57526d5a566aab8a649
vgeorge93 <georgevisa93@gmail.com>
2023-09-27 20:18:10 +0300

    Chapter 1 - building blocks

---
+++ chapter1/buildingblocks/Main.java
@@ -0,0 +1,12 @@
+package chapter1.buildingblocks;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+//See page 7 from chapter 1
+//public class Main2 { } - does not compile because we specified as an access modifier public keyword
+//private class Main2 { } - does not compile
+
+class Main2 {} //is the only version that is accepted
